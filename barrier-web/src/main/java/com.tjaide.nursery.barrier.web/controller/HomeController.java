bc847f326d2560b3f55c221294e3ea432b32adcd
xinqiongxx <xinqiongxx@163.com>
2020-06-09 17:03:47 +0800

    修改大屏页面数据

--- barrier-web/src/main/java/com.tjaide.nursery.barrier.web/controller/HomeController.java
+++ barrier-web/src/main/java/com.tjaide.nursery.barrier.web/controller/HomeController.java
@@ -62,12 +62,12 @@ public class HomeController {
         Map<String,Object> datas=sysPassProcessService.getBaseDatas();
         //获取进出人数
         Integer entercount=sysPassProcessService.count(Wrappers.<SysPassProcess>lambdaQuery()
-            .eq(SysPassProcess::getEnterType,1)
+            .eq(SysPassProcess::getEnterType,0)
             .ge(SysPassProcess::getCreateTime,starTime)
             .le(SysPassProcess::getCreateTime,endTime));
         //获取进出人数
         Integer leavecount=sysPassProcessService.count(Wrappers.<SysPassProcess>lambdaQuery()
-            .eq(SysPassProcess::getEnterType,2)
+            .eq(SysPassProcess::getEnterType,1)
             .ge(SysPassProcess::getCreateTime,starTime)
             .le(SysPassProcess::getCreateTime,endTime));
         model.addAttribute("stucount",datas.get("data1"));
